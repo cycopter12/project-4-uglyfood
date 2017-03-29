746ea826ee0162819f4a2d52f9b38d0ab9838ddf
xavier poh <xvrpke@gmail.com>
2017-03-29 09:49:20 +0800

    add devise

---
+++ db/migrate/20170328093922_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :organization_name, :string
+    add_column :users, :street_name, :string
+    add_column :users, :postal_code, :integer
+    add_column :users, :contact_number, :integer
+    add_column :users, :is_admin, :boolean, :default => false
+  end
+end
