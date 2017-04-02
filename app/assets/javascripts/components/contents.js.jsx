5d29fcc1e3c59a5aa429c68b6498381713354129
andre the <andre.the12@gmail.com>
2017-04-02 13:54:00 +0800

    deleted duplicate

--- app/assets/javascripts/components/contents.js.jsx
+++ app/assets/javascripts/components/contents.js.jsx
@@ -4,9 +4,11 @@ class Contents extends React.Component {
       <p id='notice'>
         <h1>New Contents</h1>
       </p>,
-        <form className='field'>
+        <form className='new_content' id='new_content' action='/contents' acceptCharset='UTF-8' method='post'>
+          <div>
           <label htmlfor='content_project_type'>Project type</label>
           <input type='text' name='content[project_type]' id='content_project_type' />
+          </div>
         </form>,
 
         <div className='actions'>
