cb6183ba6b33b704ed89219f971081eb97bb0c31
andre the <andre.the12@gmail.com>
2017-04-02 16:20:58 +0800

    form working

--- app/assets/javascripts/components/contents.es6.jsx
+++ app/assets/javascripts/components/contents.es6.jsx
@@ -1,19 +1,29 @@
 class Contents extends React.Component {
   render () {
     return (
-      <p id='notice'>
-        <h1>New Contents</h1>
-      </p>,
+      <div>
+        <p id='notice'>
+          <h1>New Contents</h1>
+        </p>
         <form className='new_content' id='new_content' action='/contents' acceptCharset='UTF-8' method='post'>
-        <div className="field">
-          <label htmlFor="content_project_type">Project type</label>
-          <input type="text" ref="project_type" name="content[project_type]" id="content_project_type"/>
-        </div>
-        </form>,
 
-        <div className='actions'>
-          <input type='submit' name='commit' value='Create Content' data-disable-with='Create Content' />
-        </div>
+          <div className='field'>
+            <label htmlFor='content_project_type'>Project type</label>
+            <input type='text' ref='project_type' name='content[project_type]' id='content_project_type' />
+          </div>
+
+          <div className="field">
+            <label htmlFor="content_body">Body</label>
+            <input type="text" ref='body' name="content[body]" id="content_body" />
+          </div>
+
+          <div className='actions'>
+            <input type='submit' name='commit' value='Create Content' data-disable-with='Create Content' />
+          </div>
+        </form>
+
+      </div>
+
     )
   }
 }
