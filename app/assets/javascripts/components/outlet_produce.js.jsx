60e5d788a04f63233e6e0fc4f3b5d1797f38d96f
xavier poh <xvrpke@gmail.com>
2017-04-06 10:19:19 +0800

    OP styling

--- app/assets/javascripts/components/outlet_produce.js.jsx
+++ app/assets/javascripts/components/outlet_produce.js.jsx
@@ -22,18 +22,18 @@ var OutletProduce = React.createClass({
     var quantity = this.state.editable ? <p>Quantity: <input id="qtyInput" type='text' ref='quantity' defaultValue={this.props.outletProduce.quantity} /></p> : <p>Quantity: {this.props.outletProduce.quantity}</p>
 
     return (
-      <div className="row">
-        <div className="col-md-3">
+      <div className="row" id="OProw">
+        <div className="col-md-3 OPcolumn">
         <div>{this.props.outletProduce.produce.name}</div>
         <div>${Number(this.props.outletProduce.cost_per_unit).toFixed(2)}</div>
         </div>
-        <div className="col-md-3">
+        <div className="col-md-3 OPcolumn">
         <div>{quantity}</div>
         </div>
-        <div className="col-md-3">
+        <div className="col-md-3 OPcolumn">
         ${Number(this.props.outletProduce.cost_per_unit * this.props.outletProduce.quantity).toFixed(2)}
         </div>
-        <div className="col-md-3">
+        <div className="col-md-3 OPcolumn">
         <div className="editDeleteBtn"><button id="editBtn" onClick={this.handleEdit}> {this.state.editable ? 'Submit' : 'Edit'} </button>
         <button onClick={this.props.handleDelete}> Delete </button></div>
         </div>
